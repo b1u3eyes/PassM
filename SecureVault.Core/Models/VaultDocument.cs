c1fb4c744710dd18f7c54dd810b273d2e95b0fd1
b1u3eyes <b1u3eyes1981@gmail.com>
2026-07-04 20:04:26 +0300

    Delete SecureVault.Core directory

--- SecureVault.Core/Models/VaultDocument.cs
+++
@@ -1,8 +0,0 @@
-namespace SecureVault.Core.Models;
-
-public sealed class VaultDocument
-{
-    public int SchemaVersion { get; set; } = 1;
-
-    public List<VaultEntry> Entries { get; set; } = [];
-}
